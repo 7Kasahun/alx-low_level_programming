17e848fdd56baf8ce9deff8ac29795b3ca7758d1
kassahun <kasahunayalew10@gmail.com>
2023-09-13 15:58:04 -0700

    dd

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,7 @@
-ist_digit - prints the last digit of a number
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
  * @n: the int to extract the last digit from
  * Return: value of the last digit
  */
